c27f39ef6d06e989d1f85026eb8451050e716530
Anji5499966 <72526985+Anji5499966@users.noreply.github.com>
2022-08-02 12:22:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 region = "ap-south-1"
 }
 resource "aws_instance" "example" {
-  name = "anji1"
+  #name = "anji1"
   count = 3
   ami           = "ami-076e3a557efe1aa9c"
   instance_type = "t2.micro"
