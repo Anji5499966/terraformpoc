3ddd50d718f053f902fcafbe7ee8e9ce93e24140
Anji5499966 <72526985+Anji5499966@users.noreply.github.com>
2022-08-02 13:12:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ region = "ap-south-1"
 }
 resource "aws_instance" "example" {
   #name = "anji1"
-  count = 3
+  count = 1
   ami           = "ami-076e3a557efe1aa9c"
   instance_type = "t2.micro"
   key_name = "ubuntu key"
   tags = {
-    name = "server $(count.index)"
+    name = "server1"
   }
 }
