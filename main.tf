aa4b3907e70a652ad712be19438e940c0e4094ea
Anji5499966 <72526985+Anji5499966@users.noreply.github.com>
2022-08-02 12:59:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,4 +10,7 @@ resource "aws_instance" "example" {
   ami           = "ami-076e3a557efe1aa9c"
   instance_type = "t2.micro"
   key_name = "ubuntu key"
+  tags = {
+    name = "server $(count.index)"
+  }
 }
