c471e5f43f2ead3afe80cec91efdb98b4d541684
Anji5499966 <72526985+Anji5499966@users.noreply.github.com>
2022-08-02 11:29:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,5 +7,5 @@ region = "ap-south-1"
 resource "aws_instance" "example" {
   ami           = "ami-076e3a557efe1aa9c"
   instance_type = "t2.micro"
-  Key_Name ="ubuntu key"
+  Key_name ="ubuntu key"
 }
